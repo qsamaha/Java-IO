6ee30eeacba01ceaae9f71fa7e481d6ffcd8e914
Qutada Samaha <qsamaha98@gmail.com>
2020-06-18 23:26:36 -0400

    Initial Commit

---
+++ src/com/example/IO/Location.java
@@ -0,0 +1,34 @@
+package com.example.IO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+
+    private final int locationId;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationId, String description, Map<String, Integer> exits) {
+        this.locationId = locationId;
+        this.description = description;
+        if (exits != null) {
+            this.exits = new HashMap<String, Integer>(exits);
+        } else {
+            this.exits = new HashMap<String, Integer>();
+        }
+        this.exits.put("Q", 0);
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+}
---
+++ src/com/example/IO/Main.java
@@ -0,0 +1,55 @@
+package com.example.IO;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Locations locations = new Locations();
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int loc = 1;
+        while(true){
+            System.out.println(locations.get(loc).getDescription());
+            if(loc == 0){
+                break;
+            }
+
+            Map <String, Integer> exits = locations.get(loc).getExits();
+            System.out.println("Available exits are ");
+            for(String exit: exits.keySet()){
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String east = "east";
+            String west = "west";
+            String south = "south";
+            String north = "north";
+            String quit = "quit";
+            String direction = scan.nextLine().toUpperCase();
+
+            if(direction.contains(east.toUpperCase())){
+                direction = "E";
+            }else if(direction.contains(west.toUpperCase())){
+                direction = "W";
+            }else if(direction.contains(south.toUpperCase())){
+                direction = "S";
+            }else if(direction.contains(north.toUpperCase())){
+                direction = "N";
+            }else if(direction.contains(quit.toUpperCase())){
+                direction = "Q";
+            }
+
+            if(exits.containsKey(direction)){
+                loc = exits.get(direction);
+            }else{
+                System.out.println("You cannot go in that direction");
+            }
+        }
+
+    }
+}
