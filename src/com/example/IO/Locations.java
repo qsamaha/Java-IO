297946e279408dfd1d876ddfc8a7aac31dbdbb60
Qutada Samaha <qsamaha98@gmail.com>
2020-06-19 12:25:09 -0400

    Made the fileWriter method more efficient by making my main class throw my IOExceptions

--- src/com/example/IO/Locations.java
+++ src/com/example/IO/Locations.java
@@ -11,53 +11,67 @@ public class Locations implements Map<Integer, Location> {
 
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
-    public static void main(String[] args) {
-        try{
-            FileWriter locFile = null;
-            locFile = new FileWriter("locations.txt");
-            for(Location location : locations.values()){
-                locFile.write(location.getLocationId() + " " + location.getDescription() + "\n");
+    public static void main(String[] args) throws IOException {
+//        FileWriter locFile = null;
+//        try {
+//            locFile = new FileWriter("locations.txt");
+//            for (Location location : locations.values()) {
+//                locFile.write(location.getLocationId() + " " + location.getDescription() + "\n");
+//                throw new IOException("test exception thrown while writing");
+//            }
+//        } finally {
+//            System.out.println("In finally block");
+//            if (locFile != null) {
+//                System.out.println("Attempting to close locFile");
+//                locFile.close();
+//            }
+//        }
+        try (FileWriter locFile = new FileWriter("locations.txt");
+             FileWriter difFile = new FileWriter("direction.txt")) {
+            for (Location location : locations.values()) {
+                locFile.write(location.getLocationId() + ", " + location.getDescription() + "\n");
+                for (String direction : location.getExits().keySet()) {
+                    difFile.write(location.getLocationId() + "," + direction + "," + location.getExits().get(direction) + "\n");
+
+                }
             }
-
-            locFile.close();
-        }catch(IOException e){
-            System.out.println("In catch block");
-            e.printStackTrace();
         }
-
     }
-    static{
-        Map <String, Integer> tempExit = new HashMap<String, Integer>();
 
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExit));
+    static {
+        
 
-
-        tempExit.put("W", 2);
-        tempExit.put("E", 3);
-        tempExit.put("S", 5);
-        tempExit.put("N", 2);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExit));
-
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("N", 5);
-        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
-
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("N", 1);
-        tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley besides stream", tempExit));
-
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("S", 1);
-        tempExit.put("W", 2);
-        locations.put(5, new Location(5, "You are in the forest", tempExit));
+//        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+//
+//        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExit));
+//
+//
+//        tempExit.put("W", 2);
+//        tempExit.put("E", 3);
+//        tempExit.put("S", 5);
+//        tempExit.put("N", 2);
+//        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExit));
+//
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("N", 5);
+//        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("W", 1);
+//        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
+//
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("N", 1);
+//        tempExit.put("W", 2);
+//        locations.put(4, new Location(4, "You are in a valley besides stream", tempExit));
+//
+//
+//        tempExit = new HashMap<String, Integer>();
+//        tempExit.put("S", 1);
+//        tempExit.put("W", 2);
+//        locations.put(5, new Location(5, "You are in the forest", tempExit));
 
     }
 
@@ -88,7 +102,7 @@ public class Locations implements Map<Integer, Location> {
 
     @Override
     public Location put(Integer key, Location value) {
-        return locations.put(key,value);
+        return locations.put(key, value);
     }
 
     @Override
